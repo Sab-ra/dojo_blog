bcdb123dd225863b47e399239bcec128d68c1827
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-22 21:53:34 +0300

    Add formulas for reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-        <p>{{ ninja_one.name }} -- {{ ninja_one.age }}</p>
-        <button @click="updateNinjaOne">
-          Update ninja one
-        </button>
+
+    <h2>Refs</h2> 
+    <p>{{ ninja_one.name }} -- {{ ninja_one.age }}</p>
+    <button @click="updateNinjaOne">
+      Update ninja one
+    </button>
+
+    <h2>Reactive</h2> 
+    <p>{{ ninja_two.name }} -- {{ ninja_two.age }}</p>
+    <button @click="updateNinjaTwo">
+      Update ninja two
+    </button>
+
+
   </div>
 </template>
 
@@ -17,14 +26,21 @@ export default {
 
   setup() {
     const ninja_one = ref( { name: 'mario', age: 34 } )
+    const ninja_two = reactive( { name: 'luigi', age: 35 } )
 
     const updateNinjaOne = () => {
       ninja_one.value.age = 40
     }
 
+    const updateNinjaTwo = () => {
+      ninja_two.age = 90
+    }
+
     return {
       ninja_one,
-      updateNinjaOne
+      ninja_two,
+      updateNinjaOne,
+      updateNinjaTwo
     }
   }
 }
