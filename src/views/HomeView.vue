ff6c2ae3fa155d4a4adffa215b7ef5236ae15004
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-29 06:29:55 +0300

    Input in feald updates ref variable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <input type="text" v-model="search">
     <div v-for="name in names" v-bind:key="name">{{ name }}</div>
   </div>
 </template>
@@ -12,10 +13,12 @@ export default {
   name: 'HomeView',
 
   setup() {
+    const search = ref( '' )
     const names = ref( [ 'mario', 'youshi', 'luigi', 'toad', 'bowzer', 'kamila', 'koopa', 'peach' ] )
 
     return {
-      names
+      names,
+      search
     }
 
   }
