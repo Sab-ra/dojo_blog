53bd38cdfa9b91465e48cf1b1fcb2757ac5b7dbf
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-29 06:25:42 +0300

    Loop around array of names

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-
+    <div v-for="name in names" v-bind:key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export default {
   name: 'HomeView',
 
   setup() {
+    const names = ref( [ 'mario', 'youshi', 'luigi', 'toad', 'bowzer', 'kamila', 'koopa', 'peach' ] )
 
     return {
-
+      names
     }
+
   }
+  
 }
 </script>
