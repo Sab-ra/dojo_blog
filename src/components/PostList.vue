be5a683be828c24d4a5882cb8ca30c3dfec9fc36
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-07-09 04:31:01 +0300

    Prop single posts in the list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" v-bind:key="post.id">
-      <h3>{{ post.title }}</h3>
+      <SinglePost v-bind:post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import SinglePost from './SinglePost.vue'
+
 export default {
   props: [ 'posts' ],
+  components: { SinglePost },
   setup( props ) {
     console.log( props.posts )
   }
