85a233505ad1c581fb28a3b8d0829d66481b3ee6
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-07-09 03:59:00 +0300

    Register PostList in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    
+    <PostList v-bind:posts="posts" />
   </div>
 </template>
 
 <script>
+import PostList from '@/components/PostList.vue'
 import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
-
+  components: { PostList },
   setup() {
     const posts = ref{ [
       { title: "Welcome to the blog", 
