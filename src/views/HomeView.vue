c0711c3e27a8c1336fb1d5c7fd0fadd1256dde67
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-07-10 19:49:14 +0300

    Load data with Try-Catch function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,16 +15,23 @@ export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref( [
-      { title: "Welcome to the blog", 
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        id: 1 
-      },
-      { title: "top 5 css tips",
-        body: "Lorem impsum fuck",
-        id: 2
+    const posts = ref( [ ] )
+    const error = ref( null )
+
+    const loadDataFromServer = async () => {
+      try {
+        let data = await fetch( 'http://localhost:3000/post' )
+        if( !data.ok ) {
+          throw Error( 'no data available' )
+        }
       }
-    ])
+      catch( error ) {
+        error.value = error.message
+        console.log( error.value )
+      }
+    }
+
+    loadDataFromServer()
 
     const showposts = ref( true )
     
