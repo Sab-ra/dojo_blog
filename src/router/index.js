6fa9123f1c327f1e68d6db999d2ef7bbf41f9928
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-07-12 07:33:15 +0300

    Create PostDetails view and its's rout

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PostDetailsView from '../views/PostDetailsView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/posts/:id',
+    name: 'post_details',
+    component: PostDetailsView,
+    props: true
   }
 ]
 
---
+++ src/views/PostDetalesView.vue
@@ -0,0 +1,13 @@
+<template>
+  post details 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
