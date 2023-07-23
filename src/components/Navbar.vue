a8da8ea1fa909873c44f6c0f1aeccb9d65d3a7f8
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-07-23 19:14:53 +0300

    Add navbar component to the app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <h1>
+      The Dojo Blog
+    </h1>
+    <nav>
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>
+      <router-link v-bind:to="{ name: 'createPost' }">Create Post</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0px auto;
+    padding: 10px;
+  }
+  header h1 {
+    color: #dfdfdf;
+    font-size: 48px;
+  }
+  header a {
+    color: #bbb;
+    text-decoration: none;
+    margin-left: 20px;
+  }
+  header a.router-link-active {
+    color: #444;
+    font-weight: bold;
+  }
+</style>
