eba4a6333c9ce4878f0d374df8bfb3973bd07bc5
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-08-07 21:16:47 +0300

    Add tag-clouds to tag vues

--- src/views/TagView.vue
+++ src/views/TagView.vue
@@ -3,6 +3,7 @@
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
       <PostList v-bind:posts="posts_with_tag" />
+      <TagCloud v-bind:posts="posts" />
     </div>
     <div v-else>
       <AwaitSpinner />
@@ -13,12 +14,13 @@
 <script>
 import AwaitSpinner from '../components/AwaitSpinnier.vue'
 import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
 import getPosts from '../composables/getPosts'
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 
 export default {
-  components: { AwaitSpinner, PostList },
+  components: { AwaitSpinner, PostList, TagCloud },
   setup() {
     const route = useRoute() 
     const { posts, error, loadDataFromServer } = getPosts()
