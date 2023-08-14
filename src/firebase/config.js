17a38c6f69ea6a212b0c99a867010483994b7404
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-08-15 02:43:38 +0300

    Connect to Firebase backend

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4H4mCpLtU8vb6vzVMkgLrF1zxrbmw4yM",
+  authDomain: "sabra-oy.firebaseapp.com",
+  projectId: "sabra-oy",
+  storageBucket: "sabra-oy.appspot.com",
+  messagingSenderId: "576812700930",
+  appId: "1:576812700930:web:c4373bc0b46e2133a59e1b"
+};
+
+// initialize the backend
+firebase.initializeApp( firebaseConfig )
+
+// initialize Firestore
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
