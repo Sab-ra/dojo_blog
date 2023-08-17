4aba34258913e38d0816a57608b4aee4ccf19ec7
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-08-17 18:24:12 +0300

    Get response from Firebase

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
   const posts = ref( [ ] )
@@ -6,17 +7,8 @@ const getPosts = () => {
 
   const loadDataFromServer = async () => {
     try {
-      // simulate delay for real case imitation
-      // await new Promise( resolve => {
-      //   setTimeout( resolve, 2000 )
-      // })
-
-      // fetch post data
-      let data = await fetch( 'http://localhost:3000/posts' )
-      if( !data.ok ) {
-        throw Error( 'no data available' )
-      }
-      posts.value = await data.json()
+      const getResponseFromSpecificCollection = await projectFirestore.collection( 'posts' ).get()
+      console.log( getResponseFromSpecificCollection )
     }
     catch( err ) {
       error.value = err.message
